afc374c159cd5c39abe4b360a3a862f96c68034d
Luke Dunn <lukedunn011@gmail.com>
2022-10-29 03:33:23 +1100

    finished app
    - added buttons
    - added logic
    - styled buttons and changed background
    - added score
    - uhhhh

--- color-picker-app/src/App.tsx
+++ color-picker-app/src/App.tsx
@@ -2,24 +2,24 @@ import { useEffect, useState } from 'react'
 
 import './App.css'
 
- 
-
 
 function App() {
   const [color, setColor] = useState(""); 
   const [answers, setAnswers] = useState<String[]>([]);
-
-
-
+  const [score, setScore] = useState(0);
 
 
   useEffect( () => {
-     const trueColor = getColor();
-     setColor(trueColor); 
-
-     setAnswers([trueColor, getColor(), getColor()].sort(
-      () => 0.5 - Math.random())); 
+    pickColor();
   }, []); 
+  
+
+  const pickColor = () => {
+    const trueColor = getColor();
+    setColor(trueColor); 
+    setAnswers([trueColor, getColor(), getColor()].sort(
+     () => 0.5 - Math.random()));  
+  }
     
   const getColor = () => {  
     const color = Math.floor(Math.random()*16777215).toString(16);
@@ -27,18 +27,33 @@ function App() {
   }
 
 
-
-
+  const checkAnswer = (answer: string) => {
+    if (answer === color) { 
+      setScore(score + 1);
+      pickColor();
+      return true; 
+    } else {
+      if (score == 0) {
+        setScore(0);
+      } else {
+        setScore(score - 1);
+    }
+   }
+  }
+ 
   return <div className='App'>
     <div className="guess-me" style={{background: color }}></div>
 
-  answers.map((answer = () = {
-      <button> key={answers} >{answers}</button>
-  })
-
+    <div className="answers">
+      {answers.map((answer) => (
+        <button onClick={() => checkAnswer(answer)} key={answer}> {answer} </button>   
+  ))}  
+    </div>
+    
+  <div className="Score">
+    <h1>Score: {score}</h1>
+      </div>
   </div>;
 
-  }
-
+      }
 export default App; 
-
